5e664c282610573b85cb7189af6f3e524313b1e9
SerenaDonadi <serena.donadi@slu.se>
2026-04-23 12:25:53 +0200

    gam

--- Temp_log_analyses.R
+++ Temp_log_analyses.R
@@ -15,6 +15,7 @@ library(lme4)
 library(car)
 library(visreg)
 library(ggeffects)
+library(mgcv)
 
 # Save entire workspace (creates my_workspace.RData)
 save.image(file = "my_workspace.RData")
@@ -80,9 +81,22 @@ my_site<-filter(my, Lokalnamn == "1 km S Aggarps skola") %>%
 # OBS: Frekvens..mätningar.dygn. does not correspond to the actual number of measurements in the 
 # dataset, better to calculate my own variable!
 
+my_season<-my %>%
+  mutate(
+    Månad = as.integer(Månad),
+    season = case_when(
+      Månad %in% c(12,1,2) ~ "Winter",
+      Månad %in% c(3,4,5)   ~ "Spring",
+      Månad %in% c(6,7,8)   ~ "Summer",
+      Månad %in% c(9,10,11) ~ "Autumn",
+      TRUE ~ NA_character_
+    ),
+    season_year = if_else(Månad == 12, År + 1L, År) # If the month is December, assign the observation to next year; otherwise, keep the current year.
+  ) 
+
 # aggregate
-my_day<-my %>% 
-  group_by(Lokalnamn, År, Månad, Dygn) %>%
+my_day<-my_season %>% 
+  group_by(Lokalnamn, År, Månad, Dygn,season,season_year,Instrumenttyp) %>%
   summarise(avg_day_Temperatur=mean(Temperatur ,na.rm=TRUE),
             sd_day_Temperatur = sd(Temperatur ,na.rm=TRUE),
             avg_day_Djup=mean(Djup ,na.rm=TRUE),
@@ -255,8 +269,158 @@ p_depth_scatter <- ggplot(scatter_data, aes(x = Djup, y = Temperatur, color = se
 p_depth_scatter
 
 
+
+##### add and fix time variables before modeling ####
+
+# build a "date" variable for the my_day dataset
+my_day$date <- as.Date(
+  paste(my_day$År, my_day$Månad, my_day$Dygn, sep = "-"),
+  format = "%Y-%m-%d"
+)
+class(my_day$date)
+# check for invalid dates:
+sum(is.na(my_day$date))
+
+# remove Feb 29 (so that each year has the same range, hence starting/ending point for 
+# cyclic splines, for day of the year) :
+my_day2 <- my_day[format(my_day$date, "%m-%d") != "02-29", ]
+# shift all days after Feb 28 in leap years back by 1.
+library(lubridate)
+my_day2$doy <- yday(my_day2$date)
+# Identify leap years
+is_leap <- leap_year(my_day2$date)
+# Collapse leap years to 365-day cycle
+my_day2$doy[is_leap & my_day2$doy > 59] <-my_day2$doy[is_leap & my_day2$doy > 59] - 1
+range(my_day2$doy) # check
+
+# TO EXPLORE TEMPORAL TREND
+# create a time variable that can handle missing years, Ciclicity (the vicinity of dec31 and
+# jan 1, and retain daily resolution:
+my_day2$time <- as.numeric(my_day2$date - min(my_day2$date))
+
+# TO EXPLORE SEASONALITY
+# create a variable as ordinal number for the day within a year. Use
+my_day2$doy 
+
+
 ######
 # Modelling
 ######
 
-#
+lmc <- lmeControl(niterEM = 5000,msMaxIter = 1000)
+
+unique(my_day$Lokalnamn)
+
+# make lokal as factor:
+is.factor(my_day2$Lokalnamn)
+levels(my_day2$Lokalnamn)
+table(my_day2$Lokalnamn)
+
+my_day2$Lokalnamn <- factor(my_day2$Lokalnamn)
+my_day2 <- my_day2[!is.na(my_day2$Lokalnamn), ] # there are no NAs anyways
+
+# model: s(time) → common long‑term trend,s(doy) → common seasonality,
+# s(location) → location‑specific baseline differences. This is modeled as random!
+# bs=re means: odel this variable as a random effect with shrinkage.
+# GAM equivalent of a random intercept in a mixed‑effects model. 
+# it is an average temperature offset per location. 
+
+# without corr str, and with site as random intercept
+M1<-gam(avg_day_Temperatur ~ avg_day_Djup +
+    s(time, k = 30) +
+    s(doy, bs = "cc", k = 10) +
+    s(Lokalnamn, bs = "re"),
+  data = my_day2,
+  method = "REML"
+)
+
+summary(M1)
+anova(M1) # useful when I have a factor, gives a overall F test
+plot(M1)
+
+# without corr str, and with site as random slope over time
+M2<-gam(avg_day_Temperatur ~ avg_day_Djup +
+          s(time, k = 30) +
+          s(doy, bs = "cc", k = 10) +
+          s(time, Lokalnamn, bs = "fs"),
+        data = my_day2,
+        method = "REML"
+)
+summary(M2)
+anova(M2) # useful when I have a factor, gives a overall F test
+plot(M2)
+
+# without corr str, and with site as random slope over seasonality (location-specific seasonality)
+M3<-gam(avg_day_Temperatur ~ avg_day_Djup +
+          s(time, k = 30) +
+          s(doy, bs = "cc", k = 10) +
+          s(doy, Lokalnamn, bs = "fs"),
+        data = my_day2,
+        method = "REML"
+)
+summary(M3)
+anova(M3) # useful when I have a factor, gives a overall F test
+plot(M3)
+
+
+
+
+
+
+
+# model with no temporal correl  - wrong, don't run
+M1<-gam(avg_day_Temperatur ~ avg_day_Djup*season + 
+          s(År,fx = FALSE, k = -1, bs = "cr", by = as.numeric(Lokalnamn == "1 km S Aggarps skola")) +
+          s(År,fx = FALSE, k = -1, bs = "cr", by = as.numeric(Lokalnamn == "300 m ovan väg")) +
+          s(År,fx = FALSE, k = -1, bs = "cr", by = as.numeric(Lokalnamn == "700 m uppströms Mörtsjön")) +
+          s(År,fx = FALSE, k = -1, bs = "cr", by = as.numeric(Lokalnamn == "Arålund")) +
+          s(År,fx = FALSE, k = -1, bs = "cr", by = as.numeric(Lokalnamn == "E4:an Nr 2")) +
+          s(År,fx = FALSE, k = -1, bs = "cr", by = as.numeric(Lokalnamn == "Fredriksdal")) +
+          s(År,fx = FALSE, k = -1, bs = "cr", by = as.numeric(Lokalnamn == "G:a Stenbron")) +
+          s(År,fx = FALSE, k = -1, bs = "cr", by = as.numeric(Lokalnamn == "G:a järnvägsbron")) +
+          s(År,fx = FALSE, k = -1, bs = "cr", by = as.numeric(Lokalnamn == "Gamla Kvarnen")) +
+          s(År,fx = FALSE, k = -1, bs = "cr", by = as.numeric(Lokalnamn == "Gravbacka")) +
+          s(År,fx = FALSE, k = -1, bs = "cr", by = as.numeric(Lokalnamn == "Huvudfåran")) +
+          s(År,fx = FALSE, k = -1, bs = "cr", by = as.numeric(Lokalnamn == "Hästgången")) +
+          s(År,fx = FALSE, k = -1, bs = "cr", by = as.numeric(Lokalnamn == "Jockara Fäbod")) +
+          s(År,fx = FALSE, k = -1, bs = "cr", by = as.numeric(Lokalnamn == "Kopparhemmet")) +
+          s(År,fx = FALSE, k = -1, bs = "cr", by = as.numeric(Lokalnamn == "Källsjöklack")) +
+          s(År,fx = FALSE, k = -1, bs = "cr", by = as.numeric(Lokalnamn == "Laxbäcken Övre")) +
+          s(År,fx = FALSE, k = -1, bs = "cr", by = as.numeric(Lokalnamn == "Lyckemyran")) +
+          s(År,fx = FALSE, k = -1, bs = "cr", by = as.numeric(Lokalnamn == "Munkhättan")) +
+          s(År,fx = FALSE, k = -1, bs = "cr", by = as.numeric(Lokalnamn == "Mynningsnära")) +
+          s(År,fx = FALSE, k = -1, bs = "cr", by = as.numeric(Lokalnamn == "Ned Sågen")) +
+          s(År,fx = FALSE, k = -1, bs = "cr", by = as.numeric(Lokalnamn == "Nedre")) +
+          s(År,fx = FALSE, k = -1, bs = "cr", by = as.numeric(Lokalnamn == "Nedströms Bosgårdsfallet")) +
+          s(År,fx = FALSE, k = -1, bs = "cr", by = as.numeric(Lokalnamn == "Ovan E4:an")) +
+          s(År,fx = FALSE, k = -1, bs = "cr", by = as.numeric(Lokalnamn == "Ovan väg")) +
+          s(År,fx = FALSE, k = -1, bs = "cr", by = as.numeric(Lokalnamn == "Ryerna")) +
+          s(År,fx = FALSE, k = -1, bs = "cr", by = as.numeric(Lokalnamn == "Sandån Nedre")) +
+          s(År,fx = FALSE, k = -1, bs = "cr", by = as.numeric(Lokalnamn == "Sandån övre")) +
+          s(År,fx = FALSE, k = -1, bs = "cr", by = as.numeric(Lokalnamn == "Storsele")) +
+          s(År,fx = FALSE, k = -1, bs = "cr", by = as.numeric(Lokalnamn == "Stråfulnäset övre")) +
+          s(År,fx = FALSE, k = -1, bs = "cr", by = as.numeric(Lokalnamn == "Sågverket")) +
+          s(År,fx = FALSE, k = -1, bs = "cr", by = as.numeric(Lokalnamn == "Tangådalsstugan Ö fåran")) +
+          s(År,fx = FALSE, k = -1, bs = "cr", by = as.numeric(Lokalnamn == "Vedema")) +
+          s(År,fx = FALSE, k = -1, bs = "cr", by = as.numeric(Lokalnamn == "Vid Vägen")) +
+          s(År,fx = FALSE, k = -1, bs = "cr", by = as.numeric(Lokalnamn == "Vingäng nedre")) +
+          s(År,fx = FALSE, k = -1, bs = "cr", by = as.numeric(Lokalnamn == "Västra Lövås")) +
+          s(År,fx = FALSE, k = -1, bs = "cr", by = as.numeric(Lokalnamn == "Västra Styberget")) +
+          s(År,fx = FALSE, k = -1, bs = "cr", by = as.numeric(Lokalnamn == "Ängarna")) +
+          s(År,fx = FALSE, k = -1, bs = "cr", by = as.numeric(Lokalnamn == "Åbro")) +
+          s(År,fx = FALSE, k = -1, bs = "cr", by = as.numeric(Lokalnamn == "Övre Ejgst")), 
+        method = "REML", data=my_day)
+
+summary(M1)
+anova(M1) # useful when I have a factor, gives a overall F test
+#The fx and k means that the amount of smoothing is not fixed to a predeternmined value;hence, cross-validation is used to estimate the optimal amount of smoothing. 
+# bs is for cubic regression spline to be used. try either "cr" or "cs".
+#if convergence problems: 
+# The option "control = lmc," can be used to ensure convergence
+# One option is to increase the number of iterations in the routine or reduce the convergence criteria, see the help file of gamm
+# Other options are to fix the degrees of freedom (and not use cross-validation) 
+
+# plots
+plot(M1)
+
+
